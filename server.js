09185c028ba3896e4d516192e0c916e9f5a7ce99
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-11 16:01:49 -0400

    Get Path to Resource

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-import http from 'node:http';
-import path from 'node:path';
-import { testPath } from './utils/testPath.js';
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
+
+/*
+Challenge 1:
+
+1. Get the name of the directory holding this server.js file and store it to a const ‘__dirname’.
+*/
 
 const __dirname = import.meta.dirname
 
 const server = http.createServer((req, res) => {
 
-    const absPathToResource = path.join(__dirname, 'public', 'index.html')
-    const relPathToResource = path.join('public', 'index.html')
-    console.log('absolute: ', absPathToResource)
-    console.log('relative: ', relPathToResource)
+    /*
+    Challenge 3:
+    
+    1. Import and call serveStatic and pass it the directory of this current module.
+    */
+    serveStatic(__dirname)
 
-    testPath()
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end("<html><h1>The server is working</h1></html>")
-
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><h1>The server is working</h1></html>')
 })
 
-
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
--- utils/testPath.js
+++
@@ -1,7 +0,0 @@
-import path from 'node:path'
-
-export function testPath() {
-
-    const relPathToResource = path.join('public', 'index.html')
-    console.log('testPath: ', relPathToResource)
-}
