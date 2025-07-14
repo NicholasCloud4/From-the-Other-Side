9089f4aa759b844beec982911f1dfb1deaec55ec
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-14 19:47:20 -0400

    Served the front-end

--- server.js
+++ server.js
@@ -3,14 +3,10 @@ import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
 
-
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
-
     await serveStatic(req, res, __dirname)
-
-
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
