860d4628dd515c5917d2972d14cca0a6da9abc06
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-16 17:01:00 -0400

    Server Sent Events

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -4,8 +4,10 @@ import { parseJSONBody } from '../utils/parseJSONBody.js'
 import { addNewSighting } from '../utils/addNewSighting.js'
 import { sanitizeInput } from '../utils/sanitizeInput.js'
 import { sightingEvents } from '../events/sightingEvents.js'
+import { stories } from "../data/stories.js";
 
-export async function handleGet(res) {
+
+export async function handleGet(res) { 
   const data = await getData()
   const content = JSON.stringify(data)
   sendResponse(res, 200, 'application/json', content)
@@ -18,18 +20,49 @@ export async function handlePost(req, res) {
     const sanitizedBody = sanitizeInput(parsedBody)
     await addNewSighting(sanitizedBody)
     sightingEvents.emit('sighting-added', sanitizedBody)
-    /*
-    Challenge 2
-    1. At the top of this file, import the event emitter you have created.
-    2. Use it to emit a ‘sighting-added’ event. 
-       What information does the listener function need?
-    3. Add a sighting to test!
-    */
-
     sendResponse(res, 201, 'application/json', JSON.stringify(sanitizedBody))
   } catch (err) {
     sendResponse(res, 400, 'application/json', JSON.stringify({ error: err }))
-  }
+  } 
+
+}
+
+export async function handleNews(req, res) {
+  res.statusCode = 200
+
+  res.setHeader("Content-Type", "text/event-stream")
+  res.setHeader("Cache-Control", "no-cache")
+  res.setHeader("Connection", "keep-alive")
+
+/*
+Challenge 1:
+  1. Set Content-Type, Cache-Control, and Connection headers
+*/
+
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * stories.length)
+
+    res.write(
+      `data: ${JSON.stringify({
+        event: 'news-update',
+        story: stories[randomIndex]
+      })}\n\n`
+    )
+
+/*
+Challenge 2:
+  1. Use res.write() to send an object to the frontend. 
+
+  The object should include:
+    - an event property with a descriptive name.
+    - a story chosen at random from the stories array.
+
+  Remember, the object is contained in a string which starts with 'data: '. 
+  What do you need at the end of the string to signal the end of a message block?
+
+*/
+
+  }, 3000)
 
 }
 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
-import { handleGet, handlePost } from './handlers/routeHandlers.js'
-
+import { handleGet, handlePost, handleNews } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 
@@ -10,21 +9,23 @@ const __dirname = import.meta.dirname
 const server = http.createServer(async (req, res) => {
 
     if (req.url === '/api') {
+
         if (req.method === 'GET') {
             return await handleGet(res)
         }
-        /*
-        Challenge: 
-           1. Add a route for a POST request to '/api'.
-           2. When a request comes in, pass the req and res to handlePost().
-        */
+
         else if (req.method === 'POST') {
             handlePost(req, res)
         }
 
-    }
-    else if (!req.url.startsWith('/api')) {
+    } else if (req.url === "/api/news") {
+
+        return await handleNews(req, res)
+
+    } else if (!req.url.startsWith('/api')) {
+
         return await serveStatic(req, res, __dirname)
+
     }
 })
 
